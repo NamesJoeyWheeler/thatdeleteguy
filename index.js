b0a881841c434c3dd6b4504464a5c1cdb34a115d
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2018-11-20 13:03:51 +1100

    The main shiz

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("messageDelete", (messageDelete) => {
+ messageDelete.channel.send(`DELETE`)
+});
+
+client.login(process.env.BOT_TOKEN);
